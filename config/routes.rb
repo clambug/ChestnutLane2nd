b5b3f96a7964e30094d576f68ee7ddb3a168835a
EC2 Default User <ec2-user@ip-172-31-20-89.us-east-2.compute.internal>
2018-11-25 17:47:01 +0000

    cart nearly done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'cart/index'
+  get '/cart', to: 'cart#index'
   devise_for :users
   resources :categories
   resources :products
   get 'pages/contact'
   root 'pages#about'
+  
+  get '/cart/:id', to: 'cart#add'
+  
+  get '/cart/remove/:id', to: 'cart#remove'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
