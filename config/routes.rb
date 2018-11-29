24606ecd55255c639edcd55bc1bf32e12529ea83
EC2 Default User <ec2-user@ip-172-31-20-89.us-east-2.compute.internal>
2018-11-29 17:48:58 +0000

    search and admin menu

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   
   get '/cart/remove/:id', to: 'cart#remove'
   
+  post '/search' => 'products#search'
+  
 
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
